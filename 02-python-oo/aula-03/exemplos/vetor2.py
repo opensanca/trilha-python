56d86943dd424dd4f501c48848cc9e81220b1a26
Luiz Menezes <luiz.menezesf@gmail.com>
2016-06-26 17:19:42 -0300

    sobe arquivos py-oo-aula2 + corrige exemplo py-oo-aula-3

--- 02-python-oo/aula-03/exemplos/vetor2.py
+++ 02-python-oo/aula-03/exemplos/vetor2.py
@@ -6,6 +6,25 @@ import math
 
 
 class Vetor:
+    """
+    Vetor bidimensional que implementa soma, subtração, multiplicação por escalar etc.
+
+    >>> v1 = Vetor(3, -2)
+    >>> v1
+    Vetor(3, -2)
+    >>> v2 = Vetor(1, 1)
+    >>> v2
+    Vetor(1, 1)
+    >>> v1 + v2
+    Vetor(4, 1)
+    >>> v1 - v2
+    Vetor(2, -3)
+    >>> v1 * 3
+    Vetor(9, -6)
+    >>> 5 * v2
+    Vetor(5, 5)
+    """
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
@@ -28,7 +47,11 @@ class Vetor:
             return NotImplemented
 
     def __iter__(self):
-        return iter((self.x, self.y))
+        yield self.x
+        yield self.y
+
+    def __matmul__(self, outro):
+        return self.x * outro.x + self.y * outro.y
 
     def __neg__(self):
         return self * -1
@@ -47,3 +70,27 @@ class Vetor:
 
     def __rmul__(self, outro):
         return self * outro
+
+
+class VetorMutavel(Vetor):
+    """
+    Versão mutável do Vetor bidimensional.  Não tem muita utilidade no mundo real, serve
+    mais para demonstrar como funciona `__iadd__` e `__imul__`
+    """
+
+    def __repr__(self):
+        return 'VetorMutavel({!r}, {!r})'.format(self.x, self.y)
+
+    def __iadd__(self, outro):
+        if isinstance(outro, Vetor):
+            self.x += outro.x
+            self.y += outro.y
+            return self
+        return NotImplemented
+
+    def __imul__(self, outro):
+        if isinstance(outro, Real):
+            self.x *= outro
+            self.y *= outro
+            return self
+        return NotImplemented
