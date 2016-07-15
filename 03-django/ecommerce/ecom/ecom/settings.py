2a8d6ed1f3f5a109527ac7e58e3a03474289398a
Luiz Menezes <luiz.menezesf@gmail.com>
2016-07-14 21:41:38 -0300

    change project database from sqlite to postgresql

--- 03-django/ecommerce/ecom/ecom/settings.py
+++ 03-django/ecommerce/ecom/ecom/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecom',
+        'USER': 'ecom',
+        'PASSWORD': 'ecom'
     }
 }
 
